e6ed533d087bc6e0de30b35f05fb510bc98584bf
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2017-12-10 14:24:14 +0530

    Roll dice and move to position enabled. Tracking next turn.

--- services/socketService.js
+++ services/socketService.js
@@ -1,11 +1,23 @@
 var io;
-var rooms = {}, // list of all rooms
-    allOnlinePlayers = {}; // list of all players who are currently online
 
 module.exports = function(server) {
     io = require('socket.io')(server);
     io.on('connection', _onConnection);
 };
+
+var rooms = {}, // list of all rooms
+    allOnlinePlayers = {}, // list of all players who are currently online
+    sampleRoom = {
+        // isPlaying: false,
+        players: {},
+        nextTurn: null,
+        diceRolled: false,
+        squares: null
+    },
+    samplePlayerDetails = {
+        position: 0,
+        cash: 1500
+    };
 
 function _onConnection (socket) {
     setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
@@ -18,18 +30,21 @@ function _onConnection (socket) {
 
     socket.on("HOST_GAME", hostGame);
     socket.on("JOIN_GAME", joinGame);
+    socket.on("TRIGGER_TURN", triggerTurn);
 
     // create a new room and join it
     function hostGame (data) {
         currentPlayerId = data.playerId;
 
-        // generate new currentRoomId
-        currentRoomId = "r" + Date.now() % 1000000000;
+        // generate new unique currentRoomId
+        do {
+            currentRoomId = "r" + Date.now() % 10000000;
+        } while (rooms[currentRoomId]);
 
         // create new entry in rooms for new currentRoomId if it doesn't already exist; store currentPlayerId against it
-        rooms[currentRoomId] = rooms[currentRoomId] || {};
-        rooms[currentRoomId].players = rooms[currentRoomId].players || [];
-        rooms[currentRoomId].players.push(currentPlayerId);
+        rooms[currentRoomId] = JSON.parse(JSON.stringify(sampleRoom));
+        rooms[currentRoomId].players[currentPlayerId] = JSON.parse(JSON.stringify(samplePlayerDetails));
+        rooms[currentRoomId].nextTurn = currentPlayerId;
 
         _updateAllPlayerList(currentPlayerId, currentRoomId);
 
@@ -39,7 +54,7 @@ function _onConnection (socket) {
             rooms: rooms
         });
 
-        joinSession(currentPlayerId, currentRoomId);
+        _joinSession();
     }
 
     // join room of existing player
@@ -50,12 +65,12 @@ function _onConnection (socket) {
 
         if (hostPlayerId && allOnlinePlayers[hostPlayerId] && allOnlinePlayers[hostPlayerId].rooms) {
 
-            // get current room of hostPlayerId
+            // get hostPlayerId's current room id
             currentRoomId = allOnlinePlayers[hostPlayerId].rooms[0];
 
             if (rooms[currentRoomId] && rooms[currentRoomId].players) {
 
-                rooms[currentRoomId].players.push(currentPlayerId);
+                rooms[currentRoomId].players[currentPlayerId] = JSON.parse(JSON.stringify(samplePlayerDetails));
 
                 _updateAllPlayerList(currentPlayerId, currentRoomId);
 
@@ -65,7 +80,7 @@ function _onConnection (socket) {
                     rooms: rooms
                 });
 
-                joinSession(currentPlayerId, currentRoomId);
+                _joinSession();
 
             } else {
                 socket.emit("SESSION_NOT_FOUND", {
@@ -80,15 +95,77 @@ function _onConnection (socket) {
         }
     }
 
-    // add player to room
-    function joinSession (playerId, roomId) {
+    // roll dice; move player; execute card details
+    function triggerTurn () {
+        // check if it is currentPlayerId's turn
+        if (!_isCurrentPlayersTurn()) {
+            console.log("Wrong turn!");
+            return;
+        }
+
+        // get sum of 2 dice rolls
+        let spaces = _rollDice();
+
+        // add "spaces" to player's current position; after crossing 39, player goes to 0
+        let newPosition = rooms[currentRoomId].players[currentPlayerId].position + spaces;
+        if (newPosition > 39) {
+            newPosition -= 40;
+        }
+
+        // move player to computed position
+        rooms[currentRoomId].players[currentPlayerId].position = newPosition;
+
+        socket.emit("PLAYER_MOVED", {
+            position: newPosition,
+            msg: currentPlayerId + " moves to " + newPosition
+        });
+
+        // this._executeSquare(newPosition, playerId);
+
+        // update nextTurn
+        _updateNextTurn();
+    }
+
+
+    /* Private methods */
+
+    // add current player to Socket IO room; broadcast it to all players in room
+    function _joinSession () {
         // join currentRoomId room
-        socket.join(roomId);
+        socket.join(currentRoomId);
 
         // inform everyone in currentRoomId of new joinee
-        io.sockets.in(roomId).emit("JOINED_SESSION", {
-            msg: playerId + " joined " + roomId
+        io.sockets.in(currentRoomId).emit("JOINED_SESSION", {
+            msg: currentPlayerId + " joined " + currentRoomId
         });
+    }
+
+    // check if this is current player's turn
+    function _isCurrentPlayersTurn () {
+        return rooms[currentRoomId].nextTurn === currentPlayerId;
+    }
+
+    // roll dice: get random integer between 2 & 12
+    function _rollDice () {
+        let dice1 = _getRandomInt(1, 6),
+            dice2 = _getRandomInt(1, 6);
+
+        return dice1 + dice2;
+    }
+
+    // generate random integer between "min" & "max" limits (inclusive)
+    function _getRandomInt (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    // update next turn
+    function _updateNextTurn () {
+        let playersArr = Object.keys(rooms[currentRoomId].players),
+            oldNextTurn = rooms[currentRoomId].nextTurn,
+            index = playersArr.indexOf(oldNextTurn);
+
+        rooms[currentRoomId].nextTurn = playersArr[index + 1 < playersArr.length ? index + 1 : 0];
+        console.log("Next turn:", rooms[currentRoomId].nextTurn);
     }
 }
 
